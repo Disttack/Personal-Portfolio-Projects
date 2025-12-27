fce1465e409f393cd9715ca65b8183fc75ca3498
Disttack <josephabbottiii@gmail.com>
2025-12-26 18:05:26 -0700

    hangman

---
+++ 100-days-of-python-challenge/day-7-hangman/hangman.py
@@ -0,0 +1,8 @@
+import random
+
+Letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z']
+words_list = ['python', 'java', 'kotlin', 'javascript']
+word = random.choice(words_list)
+
+
+print('Welcome to the PyHangman Game!')
